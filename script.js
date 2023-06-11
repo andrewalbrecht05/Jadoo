b380c57abf5c4f0fc9f450116fd257258bb600ea
Andrew <andrijalbrext@gmail.com>
2023-06-11 13:46:01 +0300

    fixed burger menu

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
 
-const buttonBurgerMenu = document.getElementsByClassName("burger-toggle")
+const buttonBurgerMenu = document.querySelector(".burger-toggle")
 const burgerMenu = document.querySelector(".burger-menu")
-const burgerMenuStyles = 
-`
-    display: flex
-`
+
+
 console.log("Script started!")
 console.log(buttonBurgerMenu)
 
-buttonBurgerMenu[0].addEventListener('change', () => 
-{
-    if( buttonBurgerMenu[0].checked == true)
-    {
-        
-        console.log("button checked")
-        console.log(burgerMenu)
-        
-        burgerMenu.setAttribute("style", burgerMenuStyles);
-        burgerMenu.classList.add('burger-menu--active')
-    }
-    else
-    {
-        document.getElementsByClassName("burger-menu")[0].setAttribute("style", "display: none")
-        burgerMenu.classList.remove('burger-menu--active')
-    }
-});
+buttonBurgerMenu.onclick = function(){
+    burgerMenu.classList.toggle('open')
+}
